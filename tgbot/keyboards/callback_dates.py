0b38e4ccc026753a39704108187165686684910e
Rakitov Evgenii <EARakitov@sberbank.ru>
2022-12-23 15:20:46 +0300

    для сервера

--- tgbot/keyboards/callback_dates.py
+++ tgbot/keyboards/callback_dates.py
@@ -15,3 +15,5 @@ buy_item_callback = CallbackData("buy_item", "item_id")
 
 confirm_buying_callback_balance = CallbackData("confirm_buying_balance")
 confirm_buying_callback_you_kasa = CallbackData("confirm_buying_you_kasa")
+
+confirm_cancel_adding = CallbackData("confirm_cancel_adding")
